30222545e49c1540a373da65fdc5abe8efea15de
KhaninaVera <vera.khanina.work@gmail.com>
2020-11-23 16:08:44 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import './css/core-style.css';
+import './css/bootstrap.min.css';
+import './css/classy-nav.min.css';
+import './css/nice-select.css';
+import './css/animate.css';
+import './css/font-awesome.min.css';
+import './css/magnific-popup.css';
+// import './css/owl.carousel.css';
+
+import App from './App';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById(`root`)
+);
+
+
+
